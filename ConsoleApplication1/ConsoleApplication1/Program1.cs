5aed5428dd916ab4b7ac264b6bb2176c1a75714d
m305-64\w401 <liou880620@gmail.com>
2018-04-12 18:01:16 +0800

    GOOD 作業

---
+++ ConsoleApplication1/ConsoleApplication1/Program1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 1; i <= 42; i++)
+
+            {
+                numbers.Add(i);
+            }
+            Random rand = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                int r = rand.Next(1, numbers.Count);
+                Console.Write(numbers[r]);
+                numbers.RemoveAt(r);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+    
+
